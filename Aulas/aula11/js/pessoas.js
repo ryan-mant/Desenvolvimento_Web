d24ac5913f79a35b4ae1eaddff164f3a223ea37d
ryanMantegod <ryan.tuglife@hotmail.com>
2023-06-04 11:52:52 -0300

    Adicionando aula 11

---
+++ Aulas/aula11/js/pessoas.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Pessoa {
+    constructor(nome, idade) {
+        this._nome = nome;
+        this._idade = idade;
+    }
+}
+exports.default = Pessoa;
